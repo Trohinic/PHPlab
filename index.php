a44cd54dd1258d706c694bfeac7b98d5e50ccbe4
Trohinic <trohinic@gmail.com>
2025-12-17 21:33:41 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+	<meta charset="UTF-8">
+	<title>LAB2</title>
+	<style>
+	</style>
+</head>
+
+<body>
+	<h1>Всем привет это третья лабораторная работа по PHP</h1>
+</body>
+
+<?php
+
+?>
+
+</html>
